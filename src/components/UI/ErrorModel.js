dda27919f618f9baf6b19a04b0ba5b366108070d
AkshayShah <shahaxay34@gmail.com>
2023-10-12 03:33:05 +0530

    React Portals concept is used for Backdrop and ModelOverlay

--- src/components/UI/ErrorModel.js
+++ src/components/UI/ErrorModel.js
@@ -1,14 +1,17 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import './ErrorModel.css'
 import Button from "./Button";
 import Card from "./Card";
 
 const ErrorModel = props => {
-    return (
-        <React.Fragment>
-            <div className="backdrop" onClick={props.onConfirm}>
-            </div>
+    /*Note we should keep backfrop as seperate component if used with multiple overlay, here we used with only this so we defined there only */
+    const Backdrop = props => {
+        return <div className="backdrop" onClick={props.onConfirm} />
+    }
+    const ModelOverlay = props => {
+        return (
             <Card errorModel={true}>
                 <div>
                     <header className="error-model__header">{props.errObj.title}</header>
@@ -18,6 +21,18 @@ const ErrorModel = props => {
                     </footer>
                 </div>
             </Card>
+        );
+    }
+    return (
+        <React.Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onConfirm={props.onConfirm} />,
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal(
+                <ModelOverlay errObj={props.errObj} onConfirm={props.onConfirm} />,
+                document.getElementById('overlay-root')
+            )}
         </React.Fragment>
     );
 }
