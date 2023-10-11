1056d6ce4d1f5e037beea4d9908782fc57fbfd33
AkshayShah <shahaxay34@gmail.com>
2023-10-12 03:03:50 +0530

    error model logic has been transfered from app component to user form component since same component generate and consumeerror data

--- src/App.js
+++ src/App.js
@@ -6,23 +6,13 @@ import ErrorModel from './components/UI/ErrorModel';
 
 const App=()=> {
   const [users,setUser]=useState([]);
-  const [error,setError]=useState();
-  const errorHandler=errObj=>{
-    setError({
-      title:errObj.title,
-      message:errObj.message
-    });
-  }
-  const errorConfirmHandler=()=>{
-    setError(null);
-  }
+  
   const userAddedHandler=user=>{
     setUser(prevUser=>[user,...prevUser])
   }
   return (
     <div>
-      {error && <ErrorModel errObj={error} onConfirm={errorConfirmHandler}/>}
-      <UserForm onUserAdded={userAddedHandler} onValidation={errorHandler}/>
+      <UserForm onUserAdded={userAddedHandler}/>
       <UserList users={users}/>
     </div>
   );
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,48 +1,61 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 import './UserForm.css'
 import Card from "./UI/Card";
 import Button from "./UI/Button";
+import ErrorModel from "./UI/ErrorModel";
 
-const UserForm=props=>{
-    const [name,setUsername]=useState('');
-    const [age,setAge]=useState('');
-    const usernameChangeHandler=(e)=>{
+const UserForm = props => {
+    const [name, setUsername] = useState('');
+    const [age, setAge] = useState('');
+    const usernameChangeHandler = (e) => {
         setUsername(e.target.value);
     }
-    const ageChangeHandler=(e)=>{
+    const ageChangeHandler = (e) => {
         setAge(e.target.value);
     }
-    const submissionHandler=(e)=>{
+    const submissionHandler = (e) => {
         e.preventDefault();
-        if(age.trim().length===0||name.trim().length===0){
-            props.onValidation({
-                title:'Invalid Input',
-                message:'Please enter a valid name and age (non-empty values)'
+        if (age.trim().length === 0 || name.trim().length === 0) {
+            errorHandler({
+                title: 'Invalid Input',
+                message: 'Please enter a valid name and age (non-empty values)'
             })
             return;
-        }else if(age<0){
-            props.onValidation({
-                title:'Invalid Input',
-                message:'Please enter a valid age (> 0)'
+        } else if (age < 0) {
+            errorHandler({
+                title: 'Invalid Input',
+                message: 'Please enter a valid age (> 0)'
             })
             return;
         }
-        const obj={username:name,age:age,id:Math.random().toString()};
+        const obj = { username: name, age: age, id: Math.random().toString() };
         /*reseting form */
         setUsername('');
         setAge('');
         props.onUserAdded(obj);
     }
-    
-    return(
+    /*Form validation error handling */
+    const [error, setError] = useState();
+    const errorHandler = errObj => {
+        setError({
+            title: errObj.title,
+            message: errObj.message
+        });
+    }
+    const errorConfirmHandler = () => {
+        setError(null);
+    }
+
+    return (
         <Card>
+            {error && <ErrorModel errObj={error} onConfirm={errorConfirmHandler} />}
             <form className="form" onSubmit={submissionHandler}>
                 <label for='username'>Username</label>
                 <input id="username" type="text" value={name} onChange={usernameChangeHandler}></input>
                 <label for='age'>Age(Years)</label>
                 <input id="age" type="number" value={age} onChange={ageChangeHandler}></input>
-                <Button value="Add User"/>
+                <Button value="Add User" />
             </form>
         </Card>
     );
